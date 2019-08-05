3df50c4d9fe810c51da8f7b9fdd63308173dc515
HunteRoi <titidevresse1999@hotmail.be>
2019-08-05 21:04:18 +0200

    Add Swagger

---
+++ API/Infrastructure/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace API.Infrastructure.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1,0, "alpha");
+            });
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen(options =>
+            {
+                options.OperationFilter<SwaggerDefaultValues>();
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.RoutePrefix = string.Empty;
+                options.DocumentTitle = "API Docs";
+                options.DefaultModelExpandDepth(2);
+                options.DefaultModelRendering(ModelRendering.Example);
+                //options.DefaultModelsExpandDepth(-1);
+                options.DefaultModelExpandDepth(3);
+                options.EnableDeepLinking();
+                options.ShowExtensions();
+                options.DisplayRequestDuration();
+                options.DocExpansion(DocExpansion.List);
+
+                // build a swagger endpoint for each discovered API version
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+
+            return app;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using API.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace API
 {
@@ -25,11 +26,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services
+                .AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.Formatting = Formatting.Indented)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -41,7 +47,57 @@ namespace API
                 app.UseHsts();
             }
 
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
+
+            app.UseCors(builder =>
+            {
+                builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .WithExposedHeaders("X-TotalCount", "X-PageSize", "X-PageIndex");
+            });
+
+            app.UseSwaggerDocumentation(provider);
+
+            #region Global exceptions handler
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    await Task.Run(() =>
+                    {
+                        var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        var exception = errorFeature.Error;
+
+                        var problemDetails = new ProblemDetails
+                        {
+                            Instance = $"urn:api:error:{Guid.NewGuid()}"
+                        };
+
+                        if (exception is BadHttpRequestException badHttpRequestException)
+                        {
+                            problemDetails.Title = "Invalid request";
+                            problemDetails.Status = (int)typeof(BadHttpRequestException)
+                                .GetProperty("StatusCode", BindingFlags.NonPublic | BindingFlags.Instance)
+                                .GetValue(badHttpRequestException);
+                            problemDetails.Detail = badHttpRequestException.Message;
+                        }
+                        else
+                        {
+                            problemDetails.Title = "An unexpected error occurred!";
+                            problemDetails.Status = 500;
+                            problemDetails.Detail = "Please contact us if this happens again!"; //exception.ToString();
+                        }
+
+                        context.Response.StatusCode = problemDetails.Status.Value;
+                        context.Response.WriteJson(problemDetails);
+                    });
+                });
+            });
+            #endregion
+
             app.UseMvc();
         }
     }
