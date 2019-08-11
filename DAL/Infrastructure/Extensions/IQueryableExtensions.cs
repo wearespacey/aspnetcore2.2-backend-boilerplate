f6de6329ac8fdb102ac79a4662b756c05aa8aea8
HunteRoi <titidevresse1999@hotmail.be>
2019-08-11 13:38:20 +0200

    Add paging extension to DAL

---
+++ DAL/Infrastructure/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace DAL.Infrastructure.Extensions
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> TakePage<T>(this IQueryable<T> query, int pageIndex = Constants.PAGE_INDEX, int pageSize = Constants.PAGE_SIZE)
+        {
+            return query.Skip(pageIndex * pageSize).Take(pageSize);
+        }
+    }
+}
