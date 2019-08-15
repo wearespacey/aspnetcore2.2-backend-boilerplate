1587415cb98847ab301e5c086a3d9fa29803a325
HunteRoi <titidevresse1999@hotmail.be>
2019-08-15 02:03:57 +0200

    Fit IRepository change, add docs, make all methods virtual

--- DAL/Repositories/RepositoryBase.cs
+++ DAL/Repositories/RepositoryBase.cs
@@ -2,44 +2,47 @@
 
 namespace DAL.Repositories
 {
-    public class RepositoryBase<T> : IRepository<T>
+    // Use AutoMapper to shift from T class (which is supposed to be a DTO) to a U class which is the Model entity and reverse.
+    // Google "Repository and Unit Of Work Pattern" to use repositories at their maximum power
+    // https://docs.microsoft.com/en-us/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/infrastructure-persistence-layer-implemenation-entity-framework-core
+    public class RepositoryBase<T, U> : IRepository<T, U>
     {
-        public string Create(T entity, string id)
+        public virtual string Create(T entity, string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public async Task<string> CreateAsync(T entity, string id)
+        public virtual async Task<string> CreateAsync(T entity, string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public T Read(string id)
+        public virtual T Read(string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public async Task<T> ReadAsync(string id)
+        public virtual async Task<T> ReadAsync(string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public void Update(T entity, string id)
+        public virtual void Update(T entity, string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public async Task UpdateAsync(T entity, string id)
+        public virtual async Task UpdateAsync(T entity, string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public void Delete(string id)
+        public virtual void Delete(string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public async Task DeleteAsync(string id)
+        public virtual async Task DeleteAsync(string id)
         {
             throw new System.NotImplementedException();
         }
